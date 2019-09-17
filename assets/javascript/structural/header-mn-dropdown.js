813b69eb56a40af80a928968bee420f78c83c91e
Matt Hammer <mhammer@r2integrated.com>
2019-09-17 08:51:13 -0400

    Removing old SCSS, Includes, and Assets

--- assets/javascript/structural/header-mn-dropdown.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-$(document).ready(function() {
-    //$('header nav>ul>li>a').on('focus', function() {
-    $('.header-cl nav>ul>li>a').on('focus', function() {
-        //$('header nav input[type="checkbox"]').removeProp('checked');
-        $('.header-cl nav :checked').prop('checked', false);
-    });
-});
